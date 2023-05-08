e7df9a15cac9b5d67973d076fe1e4e9a75948b85
donghee <dongheepark@gmail.com>
2023-05-08 21:59:00 +0900

    Add backup script

---
+++ tools/backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo '----'
+echo `date -I -d 'today'`
+
+HOME=/home/donghee
+PROJECT=zeromakeszero
+
+cd $HOME/src/github.com/donghee
+tar cvfz $HOME/src/github.com/donghee/${PROJECT}_`date -I -d 'today'`.tar.gz --exclude "node_modules" --exclude ".git" $PROJECT
+rclone copy -P $HOME/src/github.com/donghee/${PROJECT}_`date -I -d 'today'`.tar.gz gdrive:/backup/$PROJECT/
+rm $HOME/src/github.com/donghee/${PROJECT}_`date -I -d 'today'`.tar.gz 
