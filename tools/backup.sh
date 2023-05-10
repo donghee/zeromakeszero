61f684835c3ff670107c07b1680a7c361926a617
donghee <dongheepark@gmail.com>
2023-05-10 20:47:26 +0900

    Update backup path

--- tools/backup.sh
+++ tools/backup.sh
@@ -8,5 +8,5 @@ PROJECT=zeromakeszero
 
 cd $HOME/src/github.com/donghee
 tar cvfz $HOME/src/github.com/donghee/${PROJECT}_`date -I -d 'today'`.tar.gz --exclude "node_modules" --exclude ".git" $PROJECT
-rclone copy -P $HOME/src/github.com/donghee/${PROJECT}_`date -I -d 'today'`.tar.gz gdrive:/backup/$PROJECT/
+rclone copy -P $HOME/src/github.com/donghee/${PROJECT}_`date -I -d 'today'`.tar.gz gdrive:/backups/$PROJECT/
 rm $HOME/src/github.com/donghee/${PROJECT}_`date -I -d 'today'`.tar.gz 
